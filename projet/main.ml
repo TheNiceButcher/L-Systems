0440a8dfd286b8eb15c200341653a6365d0a865c
Harris HAMDANI <harris.hamdani@epita.fr>
2020-11-18 17:27:19 +0100

    test commit

--- projet/main.ml
+++ projet/main.ml
@@ -3,7 +3,8 @@
     Nous suggérons l'utilisation du module Arg
     http://caml.inria.fr/pub/docs/manual-ocaml/libref/Arg.html
 *)
-
+open Lsystems.Systems
+open Lsystems.Turtle
 let usage = (* Entete du message d'aide pour --help *)
   "Interpretation de L-systemes et dessins fractals"
 
@@ -17,6 +18,18 @@ let extra_arg_action = fun s -> failwith ("Argument inconnu :"^s)
 
 let main () =
   Arg.parse cmdline_options extra_arg_action usage;
+  let lsyst =
+  	let axiom = Symb "A" in
+		let rules s = match s with
+		| "A" -> Seq [Symb "A";Branch (Seq [Symb "P";Symb "A"]);Symb "A";
+		Branch (Seq [Symb "M";Symb "A"]);Symb "A"]
+		| q -> Symb q
+		in
+		let interp s = [Store] in
+	{axiom;rules;interp}
+	in
+	afficher_chaine_symbole (iteration lsyst 2);
+	print_string "\n";
   print_string "Pour l'instant je ne fais rien\n"
 
 (** On ne lance ce main que dans le cas d'un programme autonome
--- projet/systems.ml
+++ projet/systems.ml
@@ -1,5 +1,4 @@
 (** Words, rewrite systems, and rewriting *)
-
 type 's word =
   | Symb of 's
   | Seq of 's word list
@@ -13,3 +12,34 @@ type 's system = {
     interp : 's -> Turtle.command list }
 
 (** Put here any type and function implementations concerning systems *)
+let rec suivant word lsyst =
+	match word with
+	| Symb s -> lsyst.rules s
+	| Seq l -> Seq (List.map (fun w -> suivant w lsyst) l)
+	| Branch w -> Branch (suivant w lsyst);;
+let iteration lsyst i =
+	let mot = lsyst.axiom in
+		if (i = 0) then mot
+		else
+			let rec next new_word j =
+			 if (j = i) then new_word
+			 else next (suivant new_word lsyst) (j+1)
+			 in
+			 next mot 0;;
+let rec afficher_chaine_symbole word =
+	match word with
+	| Symb s -> print_string s
+	| Seq l -> begin match l with
+				| [] -> print_string ""
+				| w::ll -> (afficher_chaine_symbole w);(afficher_chaine_symbole (Seq (ll)));
+				end
+	| Branch w -> print_string "[";afficher_chaine_symbole w; print_string "]";
+(*let list_command word lsyst =
+	let rec aux l =
+		match word with
+		| Symb s -> List.rev (interp x)::l
+		| Seq li -> begin match li with
+						 | [] -> List.rev l
+						 |
+		| Branch w -> let
+*)
